4ce2e031be7ba0f28006032ba301da2e0dcd0379
dvsqwx <dvsqwx@proton.me>
2026-03-22 21:30:47 +0200

    Update asyncFilter.js

--- backend/lib/asyncFilter.js
+++ backend/lib/asyncFilter.js
@@ -1,1 +1,11 @@
+'use strict'
 
+//task 5 async
+
+export function asyncFilterCallback(array, predicate, callback) {
+    let res = []
+    let n = array.length
+    if(!array || array.length == 0) {
+        callback(null, [])
+        return
+    }
