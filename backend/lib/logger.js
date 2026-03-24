85d662624ac2af9fe2c8554e433c62a90519c77d
dvsqwx <dvsqwx@proton.me>
2026-03-24 16:11:57 +0200

    Update logger.js

--- backend/lib/logger.js
+++ backend/lib/logger.js
@@ -1,1 +1,25 @@
 'use strict'
+
+// task 9 - logger
+
+import fs from 'fs'
+import path from 'path'
+
+const LEVELS = ['debug', 'info', 'call', 'error']
+
+function formatText(level, message, meta) {
+    const time = new Date().toISOString()
+    let out = `[${time}] [${level}] ${message}`
+    if(meta) out += ' ' + JSON.stringify(meta)
+    return out
+}
+
+function formatJson(level, message, meta) {
+    const obj = {
+        time: new Date().toISOString(),
+        level,
+        message,
+        ...meta
+    }
+    return JSON.stringify(obj)
+}
