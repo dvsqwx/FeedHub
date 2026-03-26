a12420fcf409f231bd77b2b69e4b6bdfee432fdf
dvsqwx <dvsqwx@proton.me>
2026-03-27 00:36:15 +0200

    Update priorityQueue.js

--- backend/lib/priorityQueue.js
+++ backend/lib/priorityQueue.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //task 4 bi directional queue
+
 export class BiDirectionalPriorityQueue {
     constructor() {
         this.items = []
@@ -15,4 +16,20 @@ export class BiDirectionalPriorityQueue {
         }
         this.items.push(newItem)
         this.count++
+    }
+
+    dequeue(mode = 'highest') {
+        if(this.items.length == 0) return null
+
+        let bestIndex = 0
+        this.items.forEach((entry, i) => {
+            if(mode == 'highest' && entry.priority > this.items[bestIndex].priority) bestIndex = i
+            if(mode == 'lowest' && entry.priority < this.items[bestIndex].priority) bestIndex = i
+            if(mode == 'oldest' && entry.count < this.items[bestIndex].count) bestIndex = i
+            if(mode == 'newest' && entry.count > this.items[bestIndex].count) bestIndex = i
+        })
+
+        const found = this.items[bestIndex]
+        this.items.splice(bestIndex, 1)
+        return found.item
     }
