7af8a706cd76af9addd7eb9320181940aeca0370
dvsqwx <dvsqwx@proton.me>
2026-03-30 23:00:17 +0300

    Refactor asyncFilterCallback to improve clarity
    fixed

--- backend/lib/asyncFilter.js
+++ backend/lib/asyncFilter.js
@@ -3,12 +3,12 @@
 //task 5 async
 
 export function asyncFilterCallback(array, predicate, callback) {
-    let res = []
-    let n = array.length
     if(!array || array.length == 0) {
         callback(null, [])
         return
     }
+    let res = []
+    let n = array.length
 
     array.forEach(function(item, i) {
         Promise.resolve(predicate(item)).then(function(ok) {
