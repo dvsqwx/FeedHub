c2d3bbce5a6b8ed0f49bf09c0cea9183f42f09b6
dvsqwx <dvsqwx@proton.me>
2026-04-05 14:53:50 +0300

    Add asyncFilterCallback function for async filtering

--- backend/lib/asyncFilter.ts
+++ backend/lib/asyncFilter.ts
@@ -1,3 +1,13 @@
 type Callback<T> = (err: Error | null, result: T) => void
 type Predicate<T> = (item: T, callback: Callback<boolean>) => void
 type SyncPredicate<T> = (item: T) => boolean
+
+export function asyncFilterCallback<T>(
+    array: T[],
+    predicate: Predicate<T>,
+    callback: Callback<T[]>
+): void {
+    if(!array || array.length == 0) {
+        callback(null, [])
+        return
+    }
